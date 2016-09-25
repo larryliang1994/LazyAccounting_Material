bcc5289aa586e6f12955c7faf581591ff866db0a
unknown <>
2016-09-25 17:45:12 +0800

    first commit

--- app/src/main/java/com/jiubai/jiubaijz/MainActivity.java
+++ app/src/main/java/com/jiubai/jiubaijz/MainActivity.java
@@ -1,5 +1,7 @@
 package com.jiubai.jiubaijz;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,14 +11,21 @@ import android.text.style.ImageSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.webView)
     WebView mWebView;
+
+    @Bind(R.id.menu_button)
+    ImageButton imageButton;
+
+    private PopupWindow popupWindow;
+    private int barColor;
+    private int titleColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +57,9 @@ public class MainActivity extends AppCompatActivity {
 
         initWebView();
 
+        initPopupWindow();
 
+        test();
     }
 
     private void initWebView() {
@@ -63,30 +81,124 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-////        MenuItem item = menu.findItem(R.id.action_refresh);
-////        SpannableStringBuilder builder = new SpannableStringBuilder("* Login");
-////        // replace "*" with icon
-////        builder.setSpan(new ImageSpan(this, R.drawable.refresh), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-////        item.setTitle(builder);
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//
-//        if(menu.getClass().getSimpleName().equals("MenuBuilder")){
-//            try{
-//                Method m = menu.getClass().getDeclaredMethod(
-//                        "setOptionalIconsVisible", Boolean.TYPE);
-//                m.setAccessible(true);
-//                m.invoke(menu, true);
-//            }
-//            catch(NoSuchMethodException e){
-//                Log.e("info", "onMenuOpened", e);
-//            }
-//            catch(Exception e){
-//                throw new RuntimeException(e);
-//            }
-//        }
-//
-//        return true;
-//    }
+    private void initPopupWindow() {
+        popupWindow = new PopupWindow(this);
+        popupWindow.setElevation(8);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setFocusable(true);
+        popupWindow.setWidth(400);
+    }
+
+    @OnClick({R.id.menu_button})
+    public void OnClick(View view) {
+        switch (view.getId()) {
+            case R.id.menu_button:
+                popupWindow.showAsDropDown(imageButton);
+                break;
+        }
+    }
+
+    private void execute(String command) {
+        final String[] parameters = command.split("::");
+
+        if (parameters.length > 1) {
+            switch (parameters[0]) {
+                case "title":
+                    mToolbar.setTitle(parameters[1]);
+                    break;
+
+                case "titleColor":
+                    titleColor = Color.argb(
+                            Integer.valueOf(parameters[4]),
+                            Integer.valueOf(parameters[1]),
+                            Integer.valueOf(parameters[2]),
+                            Integer.valueOf(parameters[3]));
+                    mToolbar.setTitleTextColor(titleColor);
+                    break;
+
+                case "barColor":
+                    barColor = Color.argb(
+                            Integer.valueOf(parameters[4]),
+                            Integer.valueOf(parameters[1]),
+                            Integer.valueOf(parameters[2]),
+                            Integer.valueOf(parameters[3]));
+                    mToolbar.setBackgroundColor(barColor);
+                    popupWindow.setBackgroundDrawable(new ColorDrawable(barColor));
+
+                    break;
+
+                case "rightBtnUrl":
+
+                    break;
+
+                case "rightBtnStyle":
+                    setupMenuPopup(parameters);
+                    break;
+
+                case "showLeftBtn":
+                    if ("true".equals(parameters[1])) {
+                        //mToolbar.setNavigationIcon(R.drawable.back);
+                    } else {
+                        mToolbar.setNavigationIcon(null);
+                    }
+                    break;
+
+                case "qrScan":
+                    break;
+
+                case "share":
+                    break;
+
+                case "feedback":
+                    break;
+            }
+        }
+    }
+
+    private void setupMenuPopup(String[] parameters) {
+        final ArrayList<Integer> imageIds = new ArrayList<>();
+        final ArrayList<String> titles = new ArrayList<>();
+        final ArrayList<String> actions = new ArrayList<>();
+
+        final int length = Integer.valueOf(parameters[1]);
+
+        for (int i = 1; i <= length; i++) {
+            titles.add(parameters[i * 3 - 1]);
+            imageIds.add(getResources().getIdentifier(parameters[i * 3], "drawable", getPackageName()));
+            actions.add(parameters[i * 3 + 1]);
+        }
+
+        ListView listView = new ListView(this);
+
+        listView.setBackgroundColor(0);
+        MenuListAdapter adapter = new MenuListAdapter(this, imageIds, titles);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                popupWindow.dismiss();
+                String cmd = "showLeftBtn::true";
+                execute(cmd);
+            }
+        });
+        listView.setAdapter(adapter);
+
+        popupWindow.setContentView(listView);
+    }
+
+    private void test() {
+        String  cmd = "title::标题";
+        execute(cmd);
+
+        cmd = "barColor::55::55::55::255";
+        execute(cmd);
+
+        cmd = "titleColor::255::255::255::255";
+        execute(cmd);
+
+        cmd = "showLeftBtn::false";
+        execute(cmd);
+
+        cmd = "rightBtnStyle::4::分享::share::share::意见反馈::feedback::feedback::扫一扫::qr_scan::qrScan::刷新::refresh::refresh";
+        execute(cmd);
+    }
 }
---
+++ app/src/main/java/com/jiubai/jiubaijz/MenuListAdapter.java
@@ -0,0 +1,54 @@
+package com.jiubai.jiubaijz;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Larry Howell on 2016/9/23.
+ */
+
+public class MenuListAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Integer> imageIds;
+    private ArrayList<String> titles;
+
+    public MenuListAdapter(Context context, ArrayList<Integer> imageIds, ArrayList<String> titles) {
+        this.context = context;
+        this.imageIds = imageIds;
+        this.titles = titles;
+    }
+
+    @Override
+    public int getCount() {
+        return imageIds.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return titles.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return imageIds.get(i);
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View contentView = LayoutInflater.from(context).inflate(R.layout.menu_item, null);
+        ImageView imageView = (ImageView) contentView.findViewById(R.id.imageView_item);
+        TextView textView = (TextView) contentView.findViewById(R.id.textView_item);
+
+        imageView.setImageResource(imageIds.get(i));
+        textView.setText(titles.get(i));
+
+        return contentView;
+    }
+}
